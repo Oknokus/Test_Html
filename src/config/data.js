9fb9b397dbfe4d0c5b0823c82d4d08004737782b
Oknokus <oknokus@gmail.com>
2023-09-01 22:57:52 +0300

    Part2, create page Main

---
+++ src/config/data.js
@@ -0,0 +1,28 @@
+export const data = [
+    {
+        text: "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+        data: "10.02.2023"
+    },
+    {
+        text: "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+        data: "10.02.2023"
+    },
+    {
+        text: "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+        data: "10.02.2023"
+    },
+    {
+        text: "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+        data: "10.02.2023"
+    },
+    {
+        text: "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+        data: "10.02.2023"
+    },
+    {
+        text: "Payment methods: Skrill, Neteller, webmoney, Bank transfer",
+        data: "10.02.2023"
+    },
+];
+
+
